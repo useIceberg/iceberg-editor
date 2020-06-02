f5dae76873f0d3e8f39bf1b5478b3f36a91d61bb
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-06-02 19:06:15 +0800

    Add label

--- src/components/shortcut-button/index.js
+++ src/components/shortcut-button/index.js
@@ -1,9 +1,11 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { Fragment, Component, render } from '@wordpress/element';
 import { withSpokenMessages, Button, SVG, Path } from '@wordpress/components';
+import { displayShortcut } from '@wordpress/keycodes';
 
 class ShortcutButton extends Component {
 	constructor() {
@@ -42,6 +44,8 @@ class ShortcutButton extends Component {
 				<Fragment>
 					<Button
 						icon={ icon }
+						label={ __( 'Iceberg mode', 'iceberg' ) }
+						shortcut={ displayShortcut.secondary( 'i' ) }
 						onClick={ () => {
 							onToggle();
 						} }
