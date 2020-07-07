6b5a41e1e0d8b9dbf6daa4fd0112511fbca67516
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-07-08 01:03:59 +0800

    Add post slug

--- src/components/post-settings/index.js
+++ src/components/post-settings/index.js
@@ -15,32 +15,39 @@ import UpdateTitleHeight from '../utils/title-height';
  */
 import { __ } from '@wordpress/i18n';
 import { withDispatch, withSelect } from '@wordpress/data';
-import { compose } from '@wordpress/compose';
+import { compose, withInstanceId } from '@wordpress/compose';
 import { Fragment, Component, render } from '@wordpress/element';
+import { safeDecodeURIComponent, cleanForSlug } from '@wordpress/url';
 import {
 	PostFeaturedImageCheck,
-	PostFeaturedImage
+	PostFeaturedImage,
+	PostSlugCheck,
 } from '@wordpress/editor';
 import {
 	withSpokenMessages,
 	DropdownMenu,
 	Modal,
 	Button,
-	DropZone,
-	MenuGroup,
+	TextControl,
+	ExternalLink,
 	MenuItem,
 	BaseControl,
 } from '@wordpress/components';
 
 class PostSettings extends Component {
-	constructor() {
-		super(...arguments);
+	constructor( { postSlug, postTitle, postID } ) {
+		super( ...arguments );
 
-		this.addPostSettings = this.addPostSettings.bind(this);
+		this.addPostSettings = this.addPostSettings.bind( this );
+		this.setSlug = this.setSlug.bind( this );
 
 		this.state = {
 			isEnabled: false,
 			isSettingsOpen: false,
+			editedSlug:
+				safeDecodeURIComponent( postSlug ) ||
+				cleanForSlug( postTitle ) ||
+				postID,
 		};
 	}
 
@@ -52,9 +59,9 @@ class PostSettings extends Component {
 		this.addPostSettings();
 	}
 
-	onRequestClose(){
+	onRequestClose() {
 		const { isSettingsOpen } = this.state;
-		
+
 		this.setState( {
 			isSettingsOpen: ! isSettingsOpen,
 		} );
@@ -64,16 +71,29 @@ class PostSettings extends Component {
 		}
 	}
 
+	setSlug( event ) {
+		const { postSlug, onUpdateSlug } = this.props;
+		const { value } = event.target;
+
+		const editedSlug = cleanForSlug( value );
+
+		if ( editedSlug === postSlug ) {
+			return;
+		}
+
+		onUpdateSlug( editedSlug );
+	}
+
 	addPostSettings() {
 		const { isActive } = this.props;
 
 		const MoreMenuDropdown = () => {
 			return (
 				<Fragment>
-					<Button 
-						className="components-iceberg-post-settings__trigger" 
+					<Button
+						className="components-iceberg-post-settings__trigger"
 						icon="admin-generic"
-						onClick={ ()=>{
+						onClick={ () => {
 							this.onRequestClose();
 						} }
 					/>
@@ -81,10 +101,13 @@ class PostSettings extends Component {
 			);
 		};
 
-		const wrapper = document.querySelector('.edit-post-header__settings');
+		const wrapper = document.querySelector( '.edit-post-header__settings' );
 
-		if (!wrapper.classList.contains('iceberg-post-settings') && isActive) {
-			wrapper.classList.add('iceberg-post-settings');
+		if (
+			! wrapper.classList.contains( 'iceberg-post-settings' ) &&
+			isActive
+		) {
+			wrapper.classList.add( 'iceberg-post-settings' );
 			wrapper.insertAdjacentHTML(
 				'beforeend',
 				'<div id="components-iceberg-post-settings"></div>'
@@ -92,37 +115,77 @@ class PostSettings extends Component {
 
 			render(
 				<MoreMenuDropdown />,
-				document.getElementById('components-iceberg-post-settings')
+				document.getElementById( 'components-iceberg-post-settings' )
 			);
 		} else if (
-			wrapper.classList.contains('iceberg-post-settings') &&
-			!isActive
+			wrapper.classList.contains( 'iceberg-post-settings' ) &&
+			! isActive
 		) {
-			document.getElementById('components-iceberg-post-settings').remove();
-			wrapper.classList.remove('iceberg-post-settings');
+			document
+				.getElementById( 'components-iceberg-post-settings' )
+				.remove();
+			wrapper.classList.remove( 'iceberg-post-settings' );
 		}
 	}
 
 	render() {
 		const { isSettingsOpen } = this.state;
+		const { postType, postLink } = this.props;
+
+		if ( ! postType ) {
+			return null;
+		}
 
 		return (
 			<Fragment>
 				{ isSettingsOpen && (
 					<Modal
+						title={ __( 'Post Settings', 'iceberg' ) }
 						className="components-iceberg-modal components-iceberg-post-settings__content"
 						overlayClassName="components-iceberg-post-settings__overlay"
 						onRequestClose={ ( event ) => {
-							if( ! event.relatedTarget ||  ! event.relatedTarget.classList.contains( 'media-modal' ) ){
+							if (
+								! event.relatedTarget ||
+								! event.relatedTarget.classList.contains(
+									'media-modal'
+								)
+							) {
 								this.onRequestClose();
 							}
 						} }
 					>
-						<h3>{ __( 'Post Settings', 'iceberg' ) }</h3>
 						<div className="post-settings__featured-image">
 							<PostFeaturedImageCheck>
 								<PostFeaturedImage />
 							</PostFeaturedImageCheck>
+
+							<TextControl
+								label={
+									get(
+										postType,
+										[ 'label', 'name' ],
+										'Post'
+									) +
+									' ' +
+									__( 'Slug', 'iceberg' )
+								}
+								value={ this.state.editedSlug }
+								onChange={ ( event ) =>
+									this.setState( {
+										editedSlug: event.target.value,
+									} )
+								}
+								onBlur={ this.setSlug }
+							/>
+							<div className="edit-post-post-link__preview-link-container">
+								<ExternalLink
+									className="edit-post-post-link__link"
+									href={ postLink }
+									target="_blank"
+								>
+									{ postLink }
+								</ExternalLink>
+							</div>
 						</div>
 					</Modal>
 				) }
@@ -133,17 +196,34 @@ class PostSettings extends Component {
 
 export default compose([
 	withSelect((select) => {
-		const { getCurrentPostType } = select('core/editor');
+		const {
+			getCurrentPost,
+			getCurrentPostType,
+			getEditedPostAttribute,
+		} = select( 'core/editor' );
+
 		const { getPostType } = select('core');
+		const { id, link } = getCurrentPost();
 
 		return {
-			postType: getPostType(getCurrentPostType()),
+			postType: getPostType( getCurrentPostType() ),
+			postSlug: getEditedPostAttribute( 'slug' ),
+			postTitle: getEditedPostAttribute( 'title' ),
+			postLink: link,
+			postID: id,
 		};
 	}),
-	withDispatch((dispatch) => ({
-		closePostSettings() {
-			dispatch('core/editor').savePost();
-		},
-	})),
+	withDispatch((dispatch) => {
+		const { editPost } = dispatch( 'core/editor' );
+
+		return {
+			closePostSettings() {
+				dispatch( 'core/editor' ).savePost();
+			},
+			onUpdateSlug( slug ) {
+				editPost( { slug } );
+			},
+		};
+	}),
 	withSpokenMessages,
 ])(PostSettings);
