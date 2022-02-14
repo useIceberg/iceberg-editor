4ae6bbc66fd32d2fadb94e76e2d917d1eb6992ae
Rich Tabor <hi@richtabor.com>
2022-02-14 12:23:38 -0500

    Clean up plugin header

--- iceberg.php
+++ iceberg.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Plugin Name: Iceberg Editor
- * Plugin URI: https://useiceberg.com/
- * Description: Iceberg is a beautiful, flexible markdown editor for writing within the WordPress block editor. Iceberg leverages the best of WordPress, and the best of the new block editor, while getting out of your way – allowing you to focus on publishing your next post.
- * Version: 1.2.0
- * Author: Iceberg
- * Author URI: https://useiceberg.com/
- * Text Domain: iceberg
- * Tested up to: 5.8.1
- * Domain Path: /languages/
+ * Plugin Name:       Iceberg Editor
+ * Description:       Iceberg is a beautiful, flexible markdown editor for writing within the WordPress block editor. Iceberg leverages the best of WordPress, and the best of the new block editor, while getting out of your way – allowing you to focus on publishing your next post.
+ * Plugin URI:        https://useiceberg.com?utm_source=wp-plugins&utm_medium=iceberg-plugin&utm_campaign=plugin-uri
+ * Requires at least: 5.7
+ * Version:           1.2.0
+ * Author:            Iceberg
+ * Author URI:        https://useiceberg.com?utm_source=wp-plugins&utm_medium=iceberg-plugin&utm_campaign=author-uri
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       iceberg
+ * Domain Path:       /languages/
  *
- * @package Iceberg
+ * @package           Iceberg
  */
 
 defined( 'ABSPATH' ) || exit;
